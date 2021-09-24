4d42faa21d5cfa88fb7f9b5df356ed9f62644733
AmACoder <amacoder88@gmail.com>
2021-09-24 18:05:47 +0330

    update

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 blog.js ui library. in this library for use blog.js you dont need html
 
+version : 0.0.1 alpha
+
+actions :
+add : add content to element content
+rewrite
+
 ## TODO
 
 - [x] create link rel:postinfo
-- [x] create limk rel:post
+- [x] create update-elemtns func
+- [x] create class blog.js-content blog-js , postname attribute (if use link rel:post you dont need this)
 - [ ] create update func
-- [ ] create class blog.js-content blog-js , postname attribute (if use link rel:post you dont need this)
--- blog-ui.js
+++ blog-ui.js
@@ -2,7 +2,7 @@
 //copyright: AmACoder
 
 //funcs
-var setpostinfo = () => {
+var set_post_info = () => {
 
     let link_elements = document.getElementsByTagName("link");
     let path;
@@ -30,30 +30,41 @@ var setpostinfo = () => {
     }
     
 }
-var setposts = () => {
+var set_post = (post_name) => {
 
-    let link_elements = document.getElementsByTagName("link");
-    let name;
-    for(var i = 0 ; i < link_elements.length; i++)
-    {
-        var att = link_elements[i].getAttribute("rel");
-        if(att == "post") {
-            
-            name = link_elements[i].getAttribute("post-name");
-            break;
-        }
-    }
+
     let info = JSON.parse(localStorage.getItem("posts_info"));
-    let post = getPost(name,info);
+    let post = getPost(post_name,info);
     post.content.generated = genContent(post);
-    sessionStorage.setItem(name+"_post",JSON.stringify(post));
+    sessionStorage.setItem(post_name+"_post",JSON.stringify(post));
 }
+var update_elemnts = () => {
 
+    let show_elms = document.getElementsByClassName("blog.js-ui-element");
+    for(var i = 0 ; i < show_elms.length; i++) {
 
+        let elm_name = show_elms[i].getAttribute("element");
+        let sub_elm = show_elms[i].getAttribute("sub-element");
+        let post_name = show_elms[i].getAttribute("post-name");
+        let raw_post = sessionStorage.getItem(post_name+"_post");
+        let post = JSON.parse(raw_post)
 
+            if(sub_elm == null || sub_elm == "") 
+               show_elms[i].innerHTML = post[elm_name]
+            else 
+                show_elms[i].innerHTML = post[elm_name][sub_elm];
+
+            }
+        }
+var update = (postname) => {
+
+    let elms = document.getElementsByClassName("blog.js-ui-element");
+    for(let i = 0;i<elms.length;i++) 
+        elms[i].setAttribute("post-name",postname)
+    set_post(postname);
+    update_elemnts();
+
+}
 //main app body
-setpostinfo();
-setposts();
-
-
+set_post_info();
   
